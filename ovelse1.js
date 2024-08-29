f91ce82440ec82e7f22987f262d43764a5981f77
Melkorka <mebj0001@stud.kea.dk>
2024-08-29 20:52:17 +0200

    alle ovelser

--- ovelse1.js
+++
@@ -1,7 +0,0 @@
-function checkDrink(element){
-    if (element.alt === "beer" || element.alt === "snaps"){
-        console.log("Indeholder Alkohol");
-    } else{
-        console.log("Alkoholfri");
-    }
-}
